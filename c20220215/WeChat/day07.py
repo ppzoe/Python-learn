cd6f2a415a4f9694e6fcb5c49d2f0e31911828e9
zoe <384110237@qq.com>
2022-03-31 10:53:26 +0800

    1

--- c20220215/WeChat/day07.py
+++ c20220215/WeChat/day07.py
@@ -1,23 +1,37 @@
 from pywinauto.application import Application
 from c20220215.WeChat.settings import oaname
 app1 = Application(backend='uia').start("E:\Program Files (x86)\Tencent\WeChat\WeChat.exe")  # 打开应用
-app = Application(backend='uia').connect(process=13696)  # 连接应用 窗口id连接 每台电脑需从新更换
+app = Application(backend='uia').connect(path="E:\Program Files (x86)\Tencent\WeChat\WeChat.exe")   # 连接应用 窗口id连接 每台电脑需从新更换
 # 选择窗口
-dlg = app.window(class_name='WeChatMainWndForPC')
-dlg1 = app['微信']
+dlg = app.window(class_name='WeChatMainWndForPC')  # 微信窗口
+dlg1 = app.window(class_name='CefWebViewWnd')      # 公众号正文窗体
 # 选中控件
-souso = dlg1.child_window(title="搜索", control_type="Edit")
+souso = dlg.child_window(title="搜索", control_type="Edit")
 # 点击控件
 souso.click_input()
 # 控件内输入数据
-# for i in range(len(oaname)):
-#     souso.type_keys('^a').type_keys(oaname[i], with_spaces=True).type_keys('{ENTER}')
+for i in range(len(oaname)):
+    souso.type_keys('^a').type_keys(oaname[i], with_spaces=True)
 
-# 点击按钮
-leibiao = dlg1["ListBox"]  # (control_type="ListItem")
-leibiao1 = app['微信'][u"ListBox4"]
-# leibiao2 = leibiao1.children(best_match="ListItem")
-leibiao3 = leibiao1.child_window(best_match="ListItem2").window_text()
+    souso.type_keys('{ENTER}')
+
+    # 点击按钮
+    # 设置路径到会话列表 Sessions
+    Sessions = dlg[u"会话列表"]
+    # 设置路径到订阅列表 ListBox
+    ListBox = Sessions.children()[0].children()[2].children()[0].children()[0].children()[0].children()[1].children()[0]
+    # 设置路径到选中列表项目 SelectListItem
+    SelectListItem = ListBox.children()[1]
+    # 设置路径到 选中会话 SelectSession ps:也就是订阅号消息主页
+    SelectSession = SelectListItem.children()[0].children()[1].children()[2]
+    # leibiao2 = SelectSession.children()[0].children()[1]
+    # leibiao2.click_input()
+    # # 订阅号正文 SubscriptionBody
+    # SubscriptionBody = dlg1.children()[1].children()[0].children()[0]
+    # # 关闭 正文 Close SubscriptionBody
+    # Close = dlg1.children()[2].children()[0].children()[0].children()[3]
+    # Close.click_input()
+    print(SelectSession)
 
 
 # Button = leibiao1.child_window(title="人民日报", control_type="Button")
@@ -25,8 +39,6 @@ leibiao3 = leibiao1.child_window(best_match="ListItem2").window_text()
 # leibiao11 = leibiao.child_window(title="人民日报", control_type="ListItem")
 # leibiao2 = leibiao1['人民日报ListItem']
 
-# print(leibiao2)
-print(leibiao3)
 
 
 # edit = souso.child_window(title="搜索", control_type="Edit")
--- c20220215/WeChat/day08.py
+++ c20220215/WeChat/day08.py
@@ -1,11 +1,30 @@
+import time
 from pywinauto.application import Application
 from c20220215.WeChat.settings import oaname
-app1 = Application(backend='uia').start("E:\Program Files (x86)\Tencent\WeChat\WeChat.exe")  # 打开应用
-app = Application(backend='uia').connect(process=16436)  # 连接应用 窗口id连接 每台电脑需从新更换 重启电脑需要重新校验
-# 选择窗口
-dlg = app.window(class_name='WeChatMainWndForPC')
-dlg1 = app['微信']
-leibiao1 = dlg1[u"ListBox4"]
-# leibiao1.print_control_identifiers()
-a=leibiao1.child_window(control_type="Text")
-print(a)
+
+app1 = Application(backend='uia').start("E:\Program Files (x86)\Tencent\WeChat\WeChat.exe")  # 打开应用 路径
+# app = Application(backend='uia').connect(process=16436)  # 连接应用 窗口id连接 每台电脑需从新更换 重启电脑需要重新校验
+app2 = Application(backend='uia').connect(path="E:\Program Files (x86)\Tencent\WeChat\WeChat.exe")  # 连接应用 路径
+# 选择窗口 dlg 微信窗体
+dlg = app2.window(class_name='WeChatMainWndForPC')
+# 设置路径到会话列表 Sessions
+Sessions = dlg[u"会话列表"]
+# 设置路径到订阅列表 ListBox
+ListBox = Sessions.children()[0].children()[2].children()[0].children()[0].children()[0].children()[1].children()[0]
+# 设置路径到选中列表项目 SelectListItem
+SelectListItem = ListBox.children()[1]
+# 设置路径到 选中会话 SelectSession ps:也就是订阅号消息主页
+SelectSession = SelectListItem.children()[0].children()[1].children()[2]
+leibiao2 = SelectSession.children()[0].children()[0].children()[1]
+leibiao2.click_input()
+time.sleep(2)
+# 订阅号正文 SubscriptionBody
+dlg1 = app2.window(class_name='CefWebViewWnd')
+SubscriptionBody = dlg1.children()[1].children()[0].children()[0]
+# 关闭 正文 Close SubscriptionBody
+Close = dlg1.children()[2].children()[0].children()[0].children()[3]
+Close.click_input()
+# leibiao3 = dlg.child_window().window(LocalizedControlType="ListItem")  # 选择到列表项目
+# leibiao2.print_control_identifiers()
+print(SubscriptionBody)
+print(Close)
